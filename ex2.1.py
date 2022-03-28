c2414eaa5f32ff8b24ff5c637cee8ee733b167e3
Rafael Martins <102500171+rafaelcaetite@users.noreply.github.com>
2022-03-28 19:38:47 -0300

    Add files via upload

---
+++ ex2.1.py
@@ -0,0 +1,4 @@
+d=input('Qual o dia do seu nascimento?')
+m=input('Qual o mês do seu nascimento?')
+a=input('Qual o ano do seu nascimento?')
+print(f'Então você nasceu dia {d} de {m} de {a}. Correto?')
